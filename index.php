59e144649bd4c9615053a6b7e19f9901ec37d75a
Abrikos <krestonos@gmail.com>
2018-05-30 00:40:05 +0300

    write-php

--- index.php
+++ index.php
@@ -65,11 +65,14 @@
 
     <div class="jumbotron">
         <div class="container">
-            <h1>Hello, people!</h1>
+            <?php
+                $cleverWelcomeMassage = 'All the love, none of the crap!';
+                $pupCount = 50;
+            ?>
 
-            <p>This is a template for a simple marketing or informational website. It includes a large callout called the
-                hero unit and three supporting pieces of content. Use it as a starting point to create something more
-                unique.</p>
+            <h1><?php echo $cleverWelcomeMassage ?></h1>
+
+            <p>Over <?php echo $pupCount ?> pet friends!</p>
 
             <p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
         </div>
